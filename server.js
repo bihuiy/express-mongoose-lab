e808da7e6f8879ef9e21196eb0a209d2950ae820
Vivien <bihuiy7@gmail.com>
2025-07-10 18:25:14 +1000

    Initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+import express from "express";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT;
+
+// middleware
+app.use(express.urlencoded()); // Without this line, all payload data will be undefined in our routes
+app.use(morgan("dev"));
+
+// define a schema
+const songSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  artist: { type: String, required: true },
+  Album: { type: String },
+  releaseYear: String,
+});
+
+// compile the schema into a model/function
+const Song = mongoose.model("Song", songSchema);
+
+// index - display all songs, requires asynchronous database operations
+app.get("/songs", async (req, res) => {
+  try {
+    const songs = await Song.find();
+    return res.render("songs/index.ejs", {
+      allSongs: songs,
+    });
+  } catch (error) {
+    console.log(error);
+    //return res.render("Couldn't fetch songs");
+  }
+});
+
+// new - show a form to add a new song, displays a static form page that doesn’t require data from the database
+app.get("/songs/new", (req, res) => {
+  return res.render("songs/new.ejs");
+});
+
+// show - display a specific song's details, requires asynchronous database querying
+app.get("/songs/:songId", async (req, res) => {
+  try {
+    const song = await Song.findById(req.params.songId);
+    return res.render("songs/show.ejs", {
+      song: song,
+    });
+  } catch (error) {
+    console.log(error);
+    //res.render(`Couldn't fetch the song with this id: ${req.params.songId}`)
+  }
+});
+
+// create - add a new song to the list, requires asynchronous database writing
+app.post("/songs", async (req, res) => {
+  try {
+    const createdSong = await Song.create(req.body);
+    return res.redirect(`/songs/${createdSong._id}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const init = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Connected to MongoDB Atlas");
+    app.listen(port, () =>
+      console.log(`Server up and running on port ${port}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+init();
